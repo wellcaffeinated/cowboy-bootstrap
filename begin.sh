64c2537951d831f681b2268baef5401329f05342
Jasper Palfree <well.caffeinated@gmail.com>
2025-11-21 16:23:07 -0500

    fix chroot craziness, use new pubkey

--- begin.sh
+++ begin.sh
@@ -9,6 +9,14 @@ fi
 # --- CONFIG ---
 ANSIBLE_IMAGE="cowboy-bootstrap-ansible"
 PLAYBOOK="/ansible/setup.yaml"
+SSH_KEY="$HOME/.ssh/id_ed25519_ansible"
+
+# Generate an SSH key if it doesn’t exist
+if [ ! -f "$SSH_KEY" ]; then
+  ssh-keygen -t ed25519 -f "$SSH_KEY" -N ""
+  cat "${SSH_KEY}.pub" >> ~/.ssh/authorized_keys
+  chmod 600 ~/.ssh/authorized_keys
+fi
 
 apt-get install -y docker.io docker-compose-v2 docker-buildx
 
@@ -19,7 +27,7 @@ docker run --rm -it \
     --privileged \
     --network host \
     -v /:/host \
+    -v "$SSH_KEY":/root/.ssh/id_ed25519:ro \
     "$ANSIBLE_IMAGE" \
     -c community.general.chroot \
-    -e ansible_host=/host \
-    -D "$PLAYBOOK" -vvv
+    -D "$PLAYBOOK"
